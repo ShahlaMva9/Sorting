d0f52b7cb59cf43fbbe7474191a22f4516b913f4
Shahla Maharramova <78307910+ShahlaMva9@users.noreply.github.com>
2022-09-29 22:30:23 +0400

    Insertion sorting

--- script.js
+++ script.js
@@ -1,14 +1,31 @@
-const arr = [-2, 45, 0, 11, -9];
+//bubble sorting
+// const arr = [-2, 45, 0, 11, -9];
 
-function bubbleSorting(arr) {
-  for (let j = 0; j < arr.length; j++) {
-    for (let i = 0; i < arr.length; i++) {
-      if (arr[i] > arr[i + 1]) {
-        [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
-      }
-    }
-  }
-  console.log(arr);
-}
+// function bubbleSorting(arr) {
+//   for (let j = 0; j < arr.length; j++) {
+//     for (let i = 0; i < arr.length; i++) {
+//       if (arr[i] > arr[i + 1]) {
+//         [arr[i + 1], arr[i]] = [arr[i], arr[i + 1]];
+//       }
+//     }
+//   }
+// }
 
-bubbleSorting(arr);
+// bubbleSorting(arr);
+
+//insertion sorting
+// const arr = [85, 12, 59, 45, 72, 51];
+// const sortedData = [arr[0]];
+
+// function insertionSorting(arr) {
+//   let i = 0;
+
+//   while (i < arr.length - 1) {
+//     i++;
+//     const a = sortedData.filter((el) => arr[i] < el);
+//     const index = sortedData.indexOf(a[0]);
+//     sortedData.splice(index, 0, arr[i]);
+//   }
+//   console.log(sortedData);
+// }
+// insertionSorting(arr);
